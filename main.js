e2a68f12dbd90d0a18cc1161a39d47c8904a09e1
schaadt94 <schaadt_design@hotmail.com>
2018-06-05 17:33:22 +0200

    hej

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+/*MENU fra lukket til åben*/
+
+$(window).on("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+    $(".menubutton").on("click", trykPaaMenubutton);
+}
+
+function trykPaaMenubutton() {
+    console.log("tryk på menubutton");
+    $("nav").toggleClass("hidden");
+    //skift knappen frem og tilbage mellem kryds
+    $(".menubutton").toggleClass("kryds");
+}
 
