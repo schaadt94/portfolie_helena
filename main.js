05879bfef2b5f71da6f59cb34e7ab1a060ea6d95
schaadt94 <schaadt_design@hotmail.com>
2018-06-10 23:25:11 +0200

    done  med @media

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 /*MENU fra lukket til åben*/
 
+// Her finde jeg bredden på skærmen
+var bredde = $(window).width();
+console.log(bredde);
+
 $(window).on("load", sidenVises);
 
 function sidenVises() {
@@ -14,3 +18,17 @@ function trykPaaMenubutton() {
     $(".menubutton").toggleClass("kryds");
 }
 
+
+// Hvis bredde er mindre end eller det samme som 576px (mobil størrelse)
+if (bredde <= 576) {
+
+    //For hver .container laver jeg et click event
+    $(".container").click(function () {
+        console.log("klikkede på container")
+        // Finder nærmeste link og sætter som variablen "link"
+        var link = $(this).find("a").attr("href");
+
+        //Her redirecter jeg siden til linket sti
+        location.href = link;
+    });
+}
